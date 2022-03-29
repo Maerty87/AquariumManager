07fef6a55477d1e9417ede7d0b8d94b41894034a
Florian Gnadl <florian.gnadl@me.com>
2022-03-29 18:04:34 +0200

    Initial commit

---
+++ src/main/java/AquaWindow.java
@@ -0,0 +1,20 @@
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
+
+
+public class AquaWindow extends Frame implements ActionListener{
+    //Attributes
+    //Amounts of Fertilizer in millilitres
+    double nitrate;
+    double phosphate;
+    double potassium;
+    double iron;
+
+
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+
+    }
+}
